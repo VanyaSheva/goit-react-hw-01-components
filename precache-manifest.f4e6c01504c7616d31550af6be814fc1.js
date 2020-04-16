acfd2bc8004385b63c9c44c7e78aee72b1aa9e89
Vanya Shevchuk <aggressor2000@gmail.com>
2020-04-16 18:00:04 +0300

    Updates

---
+++ precache-manifest.f4e6c01504c7616d31550af6be814fc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3ed87269c284d8b8f51a3a8bdf446b3",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "6ee28755e1c52f18b9b3",
+    "url": "/goit-react-hw-01-components/static/css/main.875d2ca3.chunk.css"
+  },
+  {
+    "revision": "ea20ef73876c44ce856c",
+    "url": "/goit-react-hw-01-components/static/js/2.2f0da114.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.2f0da114.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee28755e1c52f18b9b3",
+    "url": "/goit-react-hw-01-components/static/js/main.60cdc518.chunk.js"
+  },
+  {
+    "revision": "d9a76cb1d7b1d9f55f6c",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.c560d133.js"
+  }
+]);
